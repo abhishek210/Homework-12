cbc6d71d6bd8744943ced513983c706ffe5eb968
abhishek yadav <abhishekyadav9667@gmail.com>
2023-04-28 13:57:54 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Host-link
+https://homework-12-fs-04.vercel.app/
---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Products from './Components/Products';
+
+function App() {
+  return (
+    <div className="App">
+         <h1>Product Details</h1>
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
